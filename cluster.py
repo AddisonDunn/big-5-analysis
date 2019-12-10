796f30bb374d28db87837ec756d3335d94772e2a
Addison Dunn <addisondunn@addisons-mbp.home>
2019-12-09 21:18:51 -0600

    Load data into dataframe and put personality trait averages into a table

--- cluster.py
+++ cluster.py
@@ -1,1 +1,25 @@
-print("hi")
+import pandas as pd
+import sys
+import numpy as numpy
+from sklearn.cluster import KMeans
+
+start_table = pd.read_csv("Big-5-Data/data.csv", sep='\t', encoding = 'utf-8')
+
+# print first 5 rows of data
+# print(start_table.head())
+
+pers_trait_table = start_table.iloc[:, 7:].copy()
+
+# Average 10 extraversion columns, neuroticism columns, etc. and
+#   put averages into a table.
+
+avg_pers_trait_table = pers_trait_table.groupby(numpy.arange(len(pers_trait_table.columns))//10, axis=1).mean())
+
+exit()
+
+num_clusters = 5
+
+kmeans = KMeans(n_clusters = num_clusters)
+kmeans.fit(pers_trait_table)
+
+print(kmeans.labels_)
