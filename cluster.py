93b407b40c2ddc3045e40717c4293230cf5e2e72
Addison Dunn <addisondunn@Addisons-MacBook-Pro.local>
2019-12-10 11:50:14 -0600

    Tried 5D scatter plot of all traits

--- cluster.py
+++ cluster.py
@@ -7,12 +7,23 @@ import plotly
 import matplotlib
 import plotly.graph_objs as go
 
-start_table = pd.read_csv("Big-5-Data/data.csv", sep='\t', encoding = 'utf-8')
+# ------- Functions ---------------------------------
+
+def load_csv_to_dataframe(filename):
+    return pd.read_csv(filename, sep='\t', encoding = 'utf-8')
+
+# ----------------------------------------------------
+
+data_filename = "Big-5-Data/data.csv"
+start_table = load_csv_to_dataframe(data_filename)
+
 
 # print first 5 rows of data
 # print(start_table.head())
 
-pers_trait_table = start_table.iloc[:, 7:].copy()
+# Grab just the first n rows and cut out demographic data.
+first_n_rows = 10000
+pers_trait_table = start_table.iloc[0:first_n_rows, 7:].copy()
 
 # Average 10 extraversion columns, neuroticism columns, etc. and
 #   put averages into a table.
@@ -23,13 +34,24 @@ avg_pers_trait_table.columns = ['E', 'N', 'A', 'C', 'O']
 
 print(avg_pers_trait_table.head())
 
-#------ Visualization ---------------------
+#------ Scatter Plot ---------------------
+# Scatter plotting failed due to data being too discrete
+
+#Set marker properties
+markersize = avg_pers_trait_table['C']
+markercolor = avg_pers_trait_table['O']
 
 #Make Plotly figure
 fig1 = go.Scatter3d(x=avg_pers_trait_table['E'],
                   y=avg_pers_trait_table['N'],
                   z=avg_pers_trait_table['A'],
-                  mode='markers')
+                  marker=dict(size=markersize,
+                                color=markercolor,
+                                opacity=0.9,
+                                reversescale=True,
+                                colorscale='Blues'),
+                    line=dict (width=0.02),
+                    mode='markers')
 
 #Make Plot.ly Layout
 mylayout = go.Layout(scene=dict(xaxis=dict(title="E"),
@@ -39,11 +61,14 @@ mylayout = go.Layout(scene=dict(xaxis=dict(title="E"),
 #Plot and save html
 plotly.offline.plot({"data": [fig1],
                      "layout": mylayout},
-                     auto_open=True)
+                     auto_open=True,
+                     filename=("5D Plot.html"))
 
-# ----------------------------------------
+# ------------ Visualization -----------------
 
 
+
+# --------------------------------------------
 exit()
 
 num_clusters = 5
